e2abf2280f150297e3c021f6264c26578f9ac93e
uday-124 <udaypratapsingh422@gmail.com>
2026-04-14 21:15:48 +0530

    welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     
@@ -46,7 +49,11 @@ export const signup = async (req, res) => {
                 profilePic:newUser.profilePic,
             });
 
-            //todo: send a welcome email to user
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email:", error);
+            }
         }else{
             res.status(400).json({message: "Invalid user data"});
         }
