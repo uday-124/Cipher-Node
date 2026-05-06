7952ca493b69a6a078d17ea59cb53f0b829fa6cd
uday-124 <udaypratapsingh422@gmail.com>
2026-05-06 22:31:12 +0530

    fix typo

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ function LoginPage() {
     email: "",
     password: "",
   });
-  const { login, isLogginIn } = useAuthStore();
+  const { login, isLoggingIn } = useAuthStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -71,9 +71,9 @@ function LoginPage() {
                   <button
                     className="auth-btn"
                     type="submit"
-                    disabled={isLogginIn}
+                    disabled={isLoggingIn}
                   >
-                    {isLogginIn ? (
+                    {isLoggingIn ? (
                       <LoaderIcon className="w-full h-5 animate-spin text center" />
                     ) : (
                       "Sign In"
