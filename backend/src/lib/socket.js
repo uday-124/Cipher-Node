c526cb2adbb1c547fe5b0521a16d9ad1cb2eb293
uday-124 <udaypratapsingh422@gmail.com>
2026-05-17 14:57:31 +0530

    realtime messages implemented

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -19,6 +19,11 @@ const io = new Server(server, {
 //apply authentication middleware to all socket connections
 io.use(socketAuthMiddleware);
 
+//we will use this function to check if the user is online or not
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId]
+}
+
 // this is for storing online user
 const userSocketMap = {}; //{userid:socketId}
 
